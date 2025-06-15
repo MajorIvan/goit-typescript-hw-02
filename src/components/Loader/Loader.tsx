25139f8f4e64692d962b94204a8be99c1dfcea18
Ivan Moshak <ivanmoshak11@gmail.com>
2025-06-15 16:18:52 +0300

    update

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,21 +1,25 @@
-import { ColorRing } from "react-loader-spinner";
+// import { ColorRing } from "react-loader-spinner";
 import css from "./Loader.module.css";
 
 const Loader: React.FC = () => {
-  return (
-    <div className={css.loader}>
-      <ColorRing
-        visible={true}
-        height="80"
-        width="80"
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-      />
-    </div>
-  );
+  return <div className={css.spinner} />;
 };
+
+// const Loader: React.FC = () => {
+//   return (
+//     <div className={css.loader}>
+//       <ColorRing
+//         visible={true}
+//         height="80"
+//         width="80"
+//         ariaLabel="color-ring-loading"
+//         wrapperStyle={{}}
+//         wrapperClass="color-ring-wrapper"
+//         colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+//       />
+//     </div>
+//   );
+// };
 
 // const Loader = () => {
 //   return (
